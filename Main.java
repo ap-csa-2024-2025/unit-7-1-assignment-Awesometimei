57f4c8ac508f6c2da680fcfa1ac8b1576edbbc72
Awesometimei <118484408+Awesometimei@users.noreply.github.com>
2025-02-19 18:00:20 +0000

    danny

--- Main.java
+++ Main.java
@@ -8,14 +8,25 @@ public class Main
     Scanner sc = new Scanner(System.in);
     String input = "";
     ArrayList<String> yay = new ArrayList<String>();
-
+    System.out.println("Please enter words, enter STOP to stop ");  
+    
     while (!input.equals("STOP"))
     {
-      
+      input = sc.nextLine();
+      yay.add(input);
     }
 
     System.out.println("Size: " + yay.size());
     System.out.println(yay);
     
+    if (yay.size() > 2)
+    {
+      String add = yay.get(0);
+      yay.remove(0);
+      yay.remove(yay.size()-1);
+      yay.remove(yay.size()-1);
+      yay.add(add);
+      System.out.println(yay);
+    }
   }
 }
